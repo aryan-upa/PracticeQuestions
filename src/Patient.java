1d73cd9ed8f76f8ad48232e0f8da84073ba0fece
Aryan Upadhyay <aryan.offc@gmail.com>
2021-08-31 16:24:14 +0530

    Initial commit

---
+++ src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+    static String PatientName;
+    static Double height;
+    static Double weight;
+
+    public static double computeBMI(){
+        return weight/(height*height);
+    }
+
+    public static void main(String[] args) {
+        PatientName = args[0];
+        height = Double.parseDouble(args[1]);
+        weight = Double.parseDouble(args[2]);
+        double var = computeBMI();
+        System.out.println("BMI of "+PatientName+ " is : " + var);
+    }
+}
