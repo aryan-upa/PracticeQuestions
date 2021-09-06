54db4f08931b858e6d005cf509fececed225cc1e
Aryan Upadhyay <aryan.offc@gmail.com>
2021-09-06 11:23:34 +0530

    Employee Class and Code

--- src/Employee.java
+++ src/Employee.java
@@ -4,35 +4,35 @@ public class Employee {
     String name;
     Integer DOJ;
     String Address;
-    public void add_Info(Employee obj){
+    public void add_Info(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Employee name : ");
-        obj.name = sc.nextLine();
+        name = sc.nextLine();
         System.out.print("Enter the Employee joining year : ");
-        obj.DOJ = sc.nextInt();
+        DOJ = sc.nextInt();
         String clr = sc.nextLine();
         System.out.print("Enter the Employee address : ");
-        obj.Address = sc.nextLine();
+        Address = sc.nextLine();
     }
 
-    public void show_Info(Employee obj){
-        System.out.println("Employee name : " + obj.name);
-        System.out.println("Employee joining year : " + obj.DOJ);
-        System.out.println("Employee address : " + obj.Address);
+    public void show_Info(){
+        System.out.println("Employee name : " + name);
+        System.out.println("Employee joining year : " + DOJ);
+        System.out.println("Employee address : " + Address);
     }
 
     public static void main(String[] args) {
         Employee n1 = new Employee();
         System.out.println("Enter Employee 1 Details : ");
-        n1.add_Info(n1);
+        n1.add_Info();
         Employee n2 = new Employee();
         System.out.println("Enter Employee 2 Details : ");
-        n2.add_Info(n2);
+        n2.add_Info();
         Employee n3 = new Employee();
         System.out.println("Enter Employee 3 Details : ");
-        n3.add_Info(n3);
-        n1.show_Info(n1);
-        n2.show_Info(n2);
-        n3.show_Info(n3);
+        n3.add_Info();
+        n1.show_Info();
+        n2.show_Info();
+        n3.show_Info();
     }
 }
