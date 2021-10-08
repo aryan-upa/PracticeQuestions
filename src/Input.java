f4f484a7a5de37781b896ee0efc65ad179b27cf9
Aryan Upadhyay <aryan.offc@gmail.com>
2021-10-08 14:16:17 +0530

    8 - October Questions

---
+++ src/Input.java
@@ -0,0 +1,9 @@
+public class Input {
+    public static void main(String...args) {
+        String sm = "";
+        for (String var: args) {
+            sm+=var;
+        }
+        System.out.println(sm);
+    }
+}
