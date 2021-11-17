8a894a88d6e8f74fd5f71cdb09235f7ae2b4ed81
Aryan Upadhyay <aryan.offc@gmail.com>
2021-11-17 21:36:32 +0530

    Exception Class Java

--- src/Exception.java
+++ src/Exception.java
@@ -1,3 +1,10 @@
 public class Exception {
-
+    public static void main(String[] args) {
+        int sum = 0;
+        for (String s: args) {
+            try { sum+= Integer.parseInt(s); }
+            catch (NumberFormatException n) { System.err.println(s + " not a Valid Integer!"); }
+        }
+        System.out.println("The Sum of elements is : " + sum);
+    }
 }
