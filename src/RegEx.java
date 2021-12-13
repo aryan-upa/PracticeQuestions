90892c379614873220289b48ed61984637d9779a
Aryan Upadhyay <aryan.offc@gmail.com>
2021-12-14 00:29:19 +0530

    13-Dec-2021

---
+++ src/RegEx.java
@@ -0,0 +1,7 @@
+import java.util.regex.*;
+
+public class RegEx {
+    public static void main(String[] args) {
+        System.out.println(Pattern.matches("[abc]{3,}","abc"));
+    }
+}
---
+++ src/ThreadRun.java
@@ -0,0 +1,36 @@
+public class ThreadRun extends Thread{
+    public void run() {
+        System.out.println("While I am printing something.");
+    }
+
+    public static void main(String[] args) {
+        Thread obj = Thread.currentThread();
+        System.out.println("name of main thread : " + obj.getName());
+
+        for(int i = 0; i<10; i++){
+            System.out.println("num is : "+i);
+        }
+    }
+}
+
+
+/*
+Sleep method creates a checked exception which must be caught, it gives interrupted exception which must be
+placed inside the try block and a catch block must be there to catch it.
+
+Placing a negative value inside the Sleep method also raises an exception.
+ */
+
+
+/*
+getName : returns the name of the thread object.
+setName : sets name of the object thread.
+Thread.currentThread : returns the name of the current thread in execution.
+start : method creates the block of that object in the thread execution and it implicitly calls the run method
+    defined in that class of that object.
+getPriority : Return the priority of the thread.
+    Three integer value can be returned :
+
+setPriority : Sets the priority of the thread in execution.
+
+ */
