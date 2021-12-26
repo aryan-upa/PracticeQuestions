3fdfd75ea5b79a0692c60cbb5c227a3d1b889101
Aryan Upadhyay <aryan.offc@gmail.com>
2021-12-26 12:12:22 +0530

    CodeStudy

--- src/Employee.java
+++ src/Employee.java
@@ -1,9 +1,25 @@
 import java.util.Scanner;
 
+/**
+ * Employee Class : Helps to create the objects of employee type
+ */
 public class Employee {
+    /**
+    name : String : The name of the Employee.
+     */
     String name;
+     /**
+     DOJ : Integer : The date of joining of the Employee.
+      */
     Integer DOJ;
+     /**
+     Address : String : The Address of the Employee.
+      */
     String Address;
+
+    /**
+     * This method adds creates an employee type object in the file.
+     * */
     public void add_Info(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Employee name : ");
@@ -15,12 +31,18 @@ public class Employee {
         Address = sc.nextLine();
     }
 
+    /**
+     * This method shows the info of the employee accessing the function.
+     */
     public void show_Info(){
         System.out.println("Employee name : " + name);
         System.out.println("Employee joining year : " + DOJ);
         System.out.println("Employee address : " + Address);
     }
 
+    /**
+    Main Method...
+     */
     public static void main(String[] args) {
         Employee n1 = new Employee();
         System.out.println("Enter Employee 1 Details : ");
